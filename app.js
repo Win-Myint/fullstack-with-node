b429bb398153648d85571f8fca83f56476d66134
Win <win.m179@gmail.com>
2017-04-23 11:12:29 +0100

    installed body-parser, added a POST route and create new camp route, touch new.ejs and included a form to submit new camp, tested and working perfectly

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 var app = require('express')();
-
-// set up view engine
+var bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 
+// parse incoming request's body into easier interface
+// then exposed it to 'req.body'
+app.use(bodyParser.urlencoded({extended: true}));
+
+// static campground data
+var campsData = [
+	{
+		name: 'aungnan',
+		img: 'https://www.mindmeister.com/images/download/3047364'
+	},
+	{
+		name: 'yangon',
+		img: 'http://www.wildnatureimages.com/images%203/060731-346..jpg'
+	}
+];
+
 // root route
-app.get('/', function(req,res){
+app.get('/', function(req, res){
 	res.render('landing');
 })
 
-// campground route
-app.get('/campground', function(req,res){
-	// static campground data
-	var campsdata = [
-		{
-			name: 'aungnan',
-			img: 'https://unsplash.com/search/camp?photo=mzZVGFfMOkA'
-		},
-		{
-			name: 'yangon',
-			img: 'https://unsplash.com/search/camp?photo=sK1hW5knKkw'
-		}
-	];
-	
-	res.render('campground', {campsdata: campsdata});
+// campgrounds route (get)
+app.get('/campgrounds', function(req, res){
+	res.render('campground', {campsData: campsData});
+})
+
+// campgrounds route (post)
+app.post('/campgrounds', function(req, res){
+	// get data from form 
+	var name = req.body.name;
+	var image = req.body.url;
+	var newCamp = {name: name, img: image};
+
+	// add to campsdata array
+	campsData.push(newCamp);
+
+	// rediret to campgrounds
+	res.redirect('/campgrounds');
+})
+
+// add new campgrounds route
+app.get('/campgrounds/new', function(req, res){
+	res.render('new');
 })
 
 // set up server

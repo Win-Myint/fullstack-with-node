7e5a19ecc5275e7395b3cc0b0cea91c0bae45822
Win <win.m179@gmail.com>
2017-04-23 09:50:19 +0100

    added campground routes and it is displaying static campground data for the time being

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ app.get('/', function(req,res){
 	res.render('landing');
 })
 
+// campground route
+app.get('/campground', function(req,res){
+	// static campground data
+	var campsdata = [
+		{
+			name: 'aungnan',
+			img: 'https://unsplash.com/search/camp?photo=mzZVGFfMOkA'
+		},
+		{
+			name: 'yangon',
+			img: 'https://unsplash.com/search/camp?photo=sK1hW5knKkw'
+		}
+	];
+	
+	res.render('campground', {campsdata: campsdata});
+})
+
 // set up server
 app.listen(3000, function(){
 	console.log("Listening to port 3000!")
